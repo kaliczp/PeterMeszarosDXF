c2cd0473cddd37e5df4978a70a8bcf1e2f26c2de
Péter Kalicz <kaliczp@gmail.com>
2022-07-31 06:35:01 +0200

    Named colums of data
    Az adatok oszlopainak elnevezése

--- todxf.R
+++ todxf.R
@@ -5,8 +5,11 @@ rawsecond <- read.csv2("secondfull.csv", head = FALSE)
 ## Egyesítés
 raw  <- rbind(rawfirst, rawsecond)
 
+names(raw) <- c("Ssz", "Y", "X", "Z", "Code")
+
 ## Dupla?
-which(duplicated(raw[,1]))
+which(duplicated(raw[, "Ssz"]))
 raw[644,]
 raw[raw[,1] == 4,]
 raw <- raw[-644,]
+
